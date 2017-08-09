4413a6cbf9bf5007eea2fd1d442f158af4150a73
Serafeim Maroulis <seramak@gmail.com>
2017-08-09 21:18:55 +0100

    Eager load user in ::recent

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
   scope :newest_first, -> { order(id: :desc) }
 
   def self.recent(page: 1, limit: 50)
-    includes(comments: :user).newest_first.page(page || 1).per(limit || 50)
+    includes(:user, comments: :user).newest_first.page(page || 1).per(limit || 50)
   end
 end
 
